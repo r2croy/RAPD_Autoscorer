86c6dc7846659ed8be64f6475816a478f9988efa
Ridoy Roy <73584510+r2croy@users.noreply.github.com>
2026-07-08 10:53:11 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+RAPD Auto Scorer
+Main Entry Point
+
+Author : Ridoy Roy
+Platform : Ubuntu
+GUI : Tkinter
+"""
+
+
+from src.gui import RAPDAutoScorerGUI
+
+
+def main():
+
+    app = RAPDAutoScorerGUI()
+
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
